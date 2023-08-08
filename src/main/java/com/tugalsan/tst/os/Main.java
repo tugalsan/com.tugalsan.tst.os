7d2b3483a24e855502920d7f441345fd30c83255
tugalsan karabacak <tugalsan@gmail.com>
2023-08-08 14:36:34 +0300

    .

--- src/main/java/com/tugalsan/tst/os/Main.java
+++ src/main/java/com/tugalsan/tst/os/Main.java
@@ -22,27 +22,23 @@ public class Main {
     public static void main(String... s) {
         d.cr("main", "TS_OSUtils.isWindows()", TS_OsPlatformUtils.isWindows());
         d.cr("main", "TS_OSUtils.getVersion()", TS_OsPlatformUtils.getVersion());
-        var exe = Path.of("C:\\me\\codes\\com.tugalsan\\tut\\com.tugalsan.tut.graalvm\\helloworld.exe");
-        var timer = TS_TimeElapsed.of();
 
-        runme(Duration.ofMillis(1), exe);
-        runme(Duration.ofMillis(10), exe);
-        runme(Duration.ofMillis(100), exe);
-        runme(Duration.ofMillis(1000), exe);
-
-        System.out.println("Time taken: " + timer.end().toMillis() + " milliseconds");
+        TGS_CallableType1<TS_OsProcess, TS_ThreadSyncTrigger> callable = kt -> TS_OsProcess.of(
+                "C:\\me\\codes\\com.tugalsan\\tut\\com.tugalsan.tut.graalvm\\helloworld.exe"
+        );
+        var elapsed = TS_TimeElapsed.of();
+        runme(Duration.ofMillis(1), callable, elapsed);
+        runme(Duration.ofMillis(10), callable, elapsed);
+        runme(Duration.ofMillis(100), callable, elapsed);
+        runme(Duration.ofMillis(1000), callable, elapsed);
     }
 
-    private static void runme(Duration until, Path exe) {
-        var elapsed = TS_TimeElapsed.of();
+    private static void runme(Duration until, TGS_CallableType1<TS_OsProcess, TS_ThreadSyncTrigger> callable, TS_TimeElapsed elapsed) {
         out.println("For dur: " + until);
-        TGS_CallableType1<TS_OsProcess, TS_ThreadSyncTrigger> callable = kt -> {
-            return TS_OsProcess.of(exe.toString());
-        };
         IntStream.range(0, 10).forEach(i -> {
             elapsed.restart();
             var called = TS_ThreadAsyncAwait.callSingle(null, until, callable);
-            var durElapsed = elapsed.end();
+            var durElapsed = elapsed.elapsed_now();
             var result = called.resultsForSuccessfulOnes.stream().findAny().orElse(null);
             if (result != null) {
                 out.println("  -result.output: " + result.output + " , elapsed: " + durElapsed);

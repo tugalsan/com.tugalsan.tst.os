263b1af000f864d86dffc827fc02242cb9584aef
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-09-08 10:40:04 +0300

    .

--- src/main/java/com/tugalsan/tst/os/Main.java
+++ src/main/java/com/tugalsan/tst/os/Main.java
@@ -15,7 +15,7 @@ public class Main {
     private static final TS_Log d = TS_Log.of(Main.class);
 
     //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.os
-    //java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.tst.os-1.0-SNAPSHOT-jar-with-dependencies.jar
+    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.os-1.0-SNAPSHOT-jar-with-dependencies.jar
     //java -jar target/com.tugalsan.tst.os-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String... s) {
         d.cr("main", "TS_OSUtils.isWindows()", TS_OsPlatformUtils.isWindows());

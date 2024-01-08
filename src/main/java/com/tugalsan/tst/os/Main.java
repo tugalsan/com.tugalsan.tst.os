8ca77e01c46f50a80015f56103d9be6a496dc85a
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-08 17:45:09 +0300

    .

--- src/main/java/com/tugalsan/tst/os/Main.java
+++ src/main/java/com/tugalsan/tst/os/Main.java
@@ -1,15 +1,12 @@
 package com.tugalsan.tst.os;
 
 import com.tugalsan.api.callable.client.TGS_CallableType1;
-import com.tugalsan.api.file.server.TS_DirectoryUtils;
-import com.tugalsan.api.file.server.TS_FileUtils;
 import com.tugalsan.api.log.server.TS_Log;
 import com.tugalsan.api.os.server.TS_OsPlatformUtils;
 import com.tugalsan.api.os.server.TS_OsProcess;
 import com.tugalsan.api.thread.server.async.TS_ThreadAsyncAwait;
 import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import static java.lang.System.out;
-import java.nio.file.Path;
 import java.time.Duration;
 import java.util.stream.IntStream;
 
@@ -21,17 +18,6 @@ public class Main {
     //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.os-1.0-SNAPSHOT-jar-with-dependencies.jar
     //java -jar target/com.tugalsan.tst.os-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String... s) {
-        TS_DirectoryUtils.copyDirectory(
-                Path.of("\\\\10.0.0.222\\kalite_dokumanlar\\Entegre Yönetim Sistemi"),
-                Path.of("C:\\Users\\me\\Desktop\\Entegre Yönetim Sistemi"),
-                true, true,
-                path -> TS_FileUtils.getNameLabel(path).contains(" OD")
-        );
-
-        if (true) {
-            return;
-        }
-
         d.cr("main", "TS_OSUtils.isWindows()", TS_OsPlatformUtils.isWindows());
         d.cr("main", "TS_OSUtils.getVersion()", TS_OsPlatformUtils.getVersion());
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.tugalsan.tst.os {
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.validator;
     requires com.tugalsan.api.os;
-    requires com.tugalsan.api.file;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.time;
